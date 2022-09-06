e31365aaaacc9a4a4ac34797d60245ebf1f980fd
mickeygray <mgray@nattaxexperts.com>
2022-09-06 14:26:24 -0700

    honky

--- server.js
+++ server.js
@@ -22,14 +22,12 @@ app.use(express.static(__dirname + "/public"));
 
 app.use("/api/leads", require("./routes/leads"));
 
-if (process.env.NODE_ENV === "production") {
- // Set static folder
- app.use(express.static("client/build"));
+// Set static folder
+app.use(express.static("var/www/html/build"));
 
- app.get("*", (req, res) =>
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
- );
-}
+app.get("*", (req, res) =>
+ res.sendFile(path.resolve("var", "www", "html", "build", "index.html"))
+);
 
 const PORT = process.env.PORT || 5000;
 
